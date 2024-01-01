286291df4986d5d0be6dd4fb883e32608c7d8e9b
pratik-hanmante <pratikhanmante3@gmail.com>
2024-01-01 22:16:21 +0530

    Initial commit or relevant commit message

---
+++ src/main/java/dev/pratik/movies/MoviesApplication.java
@@ -0,0 +1,18 @@
+package dev.pratik.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+99
+
+@SpringBootApplication
+
+public class MoviesApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+
+
+}
